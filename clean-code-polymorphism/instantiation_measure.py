c09fd8d8d7229a5721fd8595bc46d363533f55c6
Rob Brieler <rob.brieler@gmail.com>
2023-11-17 12:40:07 -0600

    :construction: increased number of trials for instantiation

--- clean-code-polymorphism/instantiation_measure.py
+++ clean-code-polymorphism/instantiation_measure.py
@@ -20,26 +20,26 @@ def time_instantiation(num_instances: int, constructor: Callable, num_args: int)
     ) / num_instances
 
 if __name__ == '__main__':
-    naive_square_times = time_instantiation(1_000, NaiveSquare, 1)
+    naive_square_times = time_instantiation(1_000_000, NaiveSquare, 1)
     print(f'Avg Naive square times in seconds were {naive_square_times:.10f}')
 
-    no_calc_square_times = time_instantiation(1_000, NoCalcSquare, 1)
+    no_calc_square_times = time_instantiation(1_000_000, NoCalcSquare, 1)
     print(f'Avg NoCalc square times in seconds were {no_calc_square_times:.10f}')
 
-    naive_rectangle_times = time_instantiation(1_000, NaiveRectangle, 2)
+    naive_rectangle_times = time_instantiation(1_000_000, NaiveRectangle, 2)
     print(f'Avg Naive rectangle times in seconds were {naive_rectangle_times:.10f}')
 
-    no_calc_rectangle_times = time_instantiation(1_000, NoCalcRectangle, 2)
+    no_calc_rectangle_times = time_instantiation(1_000_000, NoCalcRectangle, 2)
     print(f'Avg NoCalc rectangle times in seconds were {no_calc_rectangle_times:.10f}')
 
-    naive_triangle_times = time_instantiation(1_000, NaiveTriangle, 2)
+    naive_triangle_times = time_instantiation(1_000_000, NaiveTriangle, 2)
     print(f'Avg Naive triangle times in seconds were {naive_triangle_times:.10f}')
 
-    no_calc_triangle_times = time_instantiation(1_000, NoCalcTriangle, 2)
+    no_calc_triangle_times = time_instantiation(1_000_000, NoCalcTriangle, 2)
     print(f'Avg NoCalc triangle times in seconds were {no_calc_triangle_times:.10f}')
 
-    naive_circle_times = time_instantiation(1_000, NaiveCircle, 1)
+    naive_circle_times = time_instantiation(1_000_000, NaiveCircle, 1)
     print(f'Avg Naive circle times in seconds were {naive_circle_times:.10f}')
 
-    no_calc_circle_times = time_instantiation(1_000, NoCalcCircle, 1)
+    no_calc_circle_times = time_instantiation(1_000_000, NoCalcCircle, 1)
     print(f'Avg NoCalc circle times in seconds were {no_calc_circle_times:.10f}')
